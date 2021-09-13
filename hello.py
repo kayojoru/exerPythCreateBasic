8aa5713d06cd30a6d4c22572a582ff6b4395016d
kayojoru <89625832+kayojoru@users.noreply.github.com>
2021-09-13 04:08:40 -0600

    Submit create py files assignment
    Submit create py files assignment

---
+++ hello.py
@@ -0,0 +1,35 @@
+x = 1         # assign variable x the value 1
+y = x + 5     # assign variable y the value of x plus 5
+z = y         # assign variable z the value of yprint("Hello World!")
+
+x = 1       # integer
+x = 1.0     # decimal (floating point)
+
+x = 1
+print(type(x)) # outputs: <class 'int'>
+
+x = 1.0
+print(type(x)) # outputs: <class 'float'>
+
+x = True
+print(type(x)) # outputs: <class 'bool'>
+
+x = 'This is a string'
+print(x) # outputs: This is a string
+print(type(x)) # outputs: <class 'str'>
+y = "This is also a string"
+
+x = 'Hello' + ' ' + 'World!'
+print(x) # outputs: Hello World!
+
+print('Hello World!')
+
+print('What is your name?')
+name = input()
+print(name)
+
+x = int(input('Enter a number: '))
+print(type(x))
+
+x = 5
+print('The number is ' + str(x))
